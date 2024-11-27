559cf96d5812a840109455c79ecb826b7437dfab
Anthhhony <maldonadocampusanoanthy@gmail.com>
2024-11-27 03:03:37 -0300

    eliminacion de archivos inutiles

--- proyectoCESFAM/AppCESFAM/views.py
+++ proyectoCESFAM/AppCESFAM/views.py
@@ -56,147 +56,26 @@ def register(request):
     return render(request, 'templatesApp/registro_inicio.html')
 
 
-def lista_prestamos(request):
-    libros_disponibles = Libro.objects.filter(disponibilidad=True)
-    return render(request, 'templatesApp/prestamo.html', {'libros':libros_disponibles})
+def lista_documentos(request):
 
-def procesar_prestamo(request, libro_id):
-    libro = get_object_or_404(Libro, id=libro_id)
-    clientes = Cliente.objects.all()
-    categorias = Categoria.objects.all()
+    return render(request, 'templatesApp/documentos.html')
+
+
+def lista_asignaciones(request):
+
+    return render(request, 'templatesApp/asignaciones.html')
+
     
-    if request.method=='POST':
-        cliente_id = request.POST['cliente']
-        categoria_id = request.POST['categoria']
-        fecha_prestamo = request.POST['fecha_prestamo']
-        fecha_devolucion = request.POST['fecha_devolucion']
-        
-        cliente = get_object_or_404(Cliente, id=cliente_id)
-        categoria = get_object_or_404(Categoria, id=categoria_id)
-        
-        prestamo = Prestamo(
-            cliente=cliente,
-            libro=libro,
-            categoria=categoria,
-            fecha_prestamo=fecha_prestamo,
-            fecha_devolucion=fecha_devolucion
-        )
-        prestamo.save()
-        libro.disponibilidad=False
-        libro.save()
-        return redirect(prestamos_confirmados)
-    return render(request, 'templatesApp/procesar_prestamo.html', {
-        'libro':libro,
-        'clientes':clientes,
-        'categorias':categorias,
-    })
+def lista_instituciones(request):
 
-def prestamos_confirmados(request):
-    prestamos = Prestamo.objects.all()
-    return render(request, 'templatesApp/prestamos_confirmados.html', {'prestamos':prestamos})
+    return render(request, 'templatesApp/instituciones.html')
 
-def finalizar_prestamo(request, prestamo_id):
-    prestamo = get_object_or_404(Prestamo, id=prestamo_id)
-    if request.method == 'POST':
-        prestamo.estado = True
-        prestamo.save()
 
-        return redirect(prestamos_confirmados)
+def lista_alertas(request):
 
-def eliminar_prestamo(request, prestamo_id):
-    prestamo = get_object_or_404(Prestamo, id=prestamo_id)
-    if request.method == 'POST':
-        prestamo.delete()
-        return redirect(prestamos_confirmados)
-    return render(request, "templatesApp/eliminar_confirmacion.html", {"prestamo":prestamo})
-    
-def mostrar_libros(request):
-    libros = Libro.objects.all()
-    categoria = Categoria.objects.all()
-    return render(request, 'templatesApp/libros.html', {'libros':libros, 'categoria':categoria})
+    return render(request, 'templatesApp/alertas.html')
 
-def agregar_libro(request):
-    titulo = 'Agregar'
-    if request.method == 'POST':
-        form = LibroForm(request.POST)
-        if form.is_valid():
-            nombre_categoria = request.POST.get('nombre_categoria')
-            categoria, creada = Categoria.objects.get_or_create(nombre=nombre_categoria)
-            
-            
-            libro = form.save(commit=False)
-            libro.save()
-            libro.categorias.add(categoria)
-            form.save_m2m()
-            return redirect(mostrar_libros)
-    else:
-        form = LibroForm()
-        categorias = Categoria.objects.all()
-    return render(request, 'templatesApp/form_libro.html', {
-        'form': form,
-        'categorias':categorias,
-        'titulo':titulo
-    })
 
-def editar_libro(request, pk):
-    libro = get_object_or_404(Libro, pk=pk)
-    titulo = 'Editar'
-    if request.method == 'POST':
-        form = LibroForm(request.POST, instance=libro)
-        if form.is_valid():
-            form.save()
-            return redirect(mostrar_libros)
-    else:
-        form = LibroForm(instance=libro)
-    
-    return render(request, 'templatesApp/form_libro.html', {'form': form, 'libro': libro, 'titulo':titulo})
-
-def eliminar_libro(request, pk):
-    libro = get_object_or_404(Libro, pk=pk)
-    if request.method == 'POST':
-        libro.delete()
-        return redirect(mostrar_libros)
-    
-    return render(request, 'templatesApp/eliminar_confirmacion.html', {'libro': libro})
-
-def mostrar_clientes(request):
-    cliente = Cliente.objects.all()
-    return render(request, 'templatesApp/clientes.html', {'cliente':cliente})
-
-def agregar_cliente(request):
-    titulo = 'Agregar'
-    form = ClienteForm()
-    
-    if request.method == 'POST':
-        form = ClienteForm(request.POST)
-        if form.is_valid():
-            cliente = form.save(commit=False)
-            cliente.save()
-            return redirect(mostrar_clientes)
-        
-    return render(request, 'templatesApp/form_cliente.html', {
-        'form':form,
-        'titulo':titulo
-    })
-
-def editar_cliente(request, pk):
-    titulo = 'Editar'
-    cliente = Cliente.objects.get(id=pk)
-    if request.method == 'POST':
-        form = ClienteForm(request.POST, instance=cliente)
-        if form.is_valid():
-            form.save()
-            return redirect(mostrar_clientes)
-    else:
-        form = ClienteForm(instance=cliente)
-    return render(request, 'templatesApp/form_cliente.html', {'form':form,'cliente':cliente, 'titulo':titulo})
-
-def eliminar_cliente(request, pk):
-    cliente = get_object_or_404(Cliente, pk=pk)
-    if request.method == 'POST':
-        cliente.delete()
-        return redirect(mostrar_clientes)
-    return render(request, 'templatesApp/eliminar_confirmacion.html', {'cliente':cliente})
 
     
 
